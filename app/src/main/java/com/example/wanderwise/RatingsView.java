358007e2a0ebba4d60c2fa513646719251ec89d6
Hanshal Dabbiru <hanshal.dabbiru@outlook.com>
2023-04-23 03:48:36 -0700

    added second activity

--- app/src/main/java/com/example/wanderwise/RatingsView.java
+++ app/src/main/java/com/example/wanderwise/RatingsView.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.w3c.dom.Text;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -24,7 +29,7 @@ import java.util.Map;
 public class RatingsView extends AppCompatActivity {
 
     Business[] business_obj_list = new Business[20];
-\
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,8 @@ public class RatingsView extends AppCompatActivity {
         String parameters = intent.getStringExtra("parameters");
         getUnderratedBusinesses(parameters);
         getOverratedBusiness(parameters);
+        TextView t = findViewById(R.id.name1);
+        t.setText("here");
     }
 
 
@@ -59,7 +66,7 @@ public class RatingsView extends AppCompatActivity {
     public void getUnderratedBusinesses(String parameters)
     {
         String url = "https://api.yelp.com/v3/businesses/search?location=" + parameters + "&sort_by=rating&limit=20";
-        RequestQueue queue = Volley.newRequestQueue(this);
+        RequestQueue queue = Volley.newRequestQueue(RatingsView.this);
         JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, response ->
         {
             try
@@ -81,7 +88,14 @@ public class RatingsView extends AppCompatActivity {
             {
                 throw new RuntimeException(e);
             }
-        }, null)
+        }, new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            TextView t = findViewById(R.id.name1);
+            t.setText("That didn't work!");
+            }
+        })
         {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError
@@ -104,7 +118,26 @@ public class RatingsView extends AppCompatActivity {
 
     public void setUnderratedUI(Business[] underrated)
     {
-
+        int[] names = {R.id.name1, R.id.name2, R.id.name3, R.id.name4, R.id.name5};
+        int[] ratingbar = {R.id.ratingbar1, R.id.ratingBar2, R.id.ratingBar3, R.id.ratingBar4, R.id.ratingBar5};
+        int[] links = {R.id.link1, R.id.link2, R.id.link3, R.id.link4, R.id.link5};
+        int [] images = {R.id.image1, R.id.image2, R.id.image3, R.id.image4, R.id.image5};
+        int [] ratings = {R.id.rating_1, R.id.rating_2, R.id.rating_3, R.id.rating_4, R.id.rating_5};
+        for(int i = 0; i < 5; i++)
+        {
+            Button b = findViewById(R.id.button);
+            b.setText("testing");
+            TextView test = findViewById(R.id.name1);
+            test.setText("why is the not working");
+            TextView tv = findViewById(names[i]);
+            tv.setText(underrated[i].name);
+            RatingBar rb = findViewById(ratingbar[i]);
+            rb.setRating((float) underrated[i].rating);
+            TextView link = findViewById(links[i]);
+            link.setText(underrated[i].link);
+            TextView rating = findViewById(ratings[i]);
+            rating.setText((int) underrated[i].rating);
+        }
     }
 
 
@@ -127,6 +160,8 @@ public class RatingsView extends AppCompatActivity {
                     Business b = new Business(name, rating, num_ratings, link, image);
                     business_obj_list[i] = b;
                 }
+                TextView t = findViewById(R.id.name1);
+                t.setText("test plsss");
                 Business[] overrated_businesses = selectOverratedBusinesses(business_obj_list);
                 setOverratedUI(overrated_businesses);
 
@@ -159,12 +194,23 @@ public class RatingsView extends AppCompatActivity {
     {
         int[] names = {R.id.name1, R.id.name2, R.id.name3, R.id.name4, R.id.name5};
         int[] ratingbar = {R.id.ratingbar1, R.id.ratingBar2, R.id.ratingBar3, R.id.ratingBar4, R.id.ratingBar5};
+        int[] links = {R.id.link1, R.id.link2, R.id.link3, R.id.link4, R.id.link5};
+        int [] images = {R.id.image1, R.id.image2, R.id.image3, R.id.image4, R.id.image5};
+        int [] ratings = {R.id.rating_1, R.id.rating_2, R.id.rating_3, R.id.rating_4, R.id.rating_5};
         for(int i = 0; i < 5; i++)
         {
+            Button b = findViewById(R.id.button);
+            b.setText("testing");
+            TextView test = findViewById(R.id.name1);
+            test.setText("why is the not working");
             TextView tv = findViewById(names[i]);
             tv.setText(overrated[i].name);
             RatingBar rb = findViewById(ratingbar[i]);
             rb.setRating((float) overrated[i].rating);
+            TextView link = findViewById(links[i]);
+            link.setText(overrated[i].link);
+            TextView rating = findViewById(ratings[i]);
+            rating.setText((int) overrated[i].rating);
         }
     }
 }
